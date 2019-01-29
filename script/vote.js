fb6435c4dc5b3a2653944e6e967916b1added968
Nabeelah Yousuph <noblenab13@gmail.com>
2019-01-29 13:45:54 +0100

    Corrected vote.js file

--- script/vote.js
+++ script/vote.js
@@ -1,13 +1,10 @@
 document.getElementById("offices").addEventListener("change", function() {
 	if(this.value === "president"){
 		document.getElementById("president").style.display = "block";
-	}else {
-		document.getElementById("president").style.display = "none";
-	}
-
-	if(this.value === "governor"){
+	}else if (this.value === "governor") {
 		document.getElementById("governor").style.display = "block";
 	}else {
+		document.getElementById("president").style.display = "none";
 		document.getElementById("governor").style.display = "none";
 	}
 });
