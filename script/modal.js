9134b9356fc05db6de1526a104139d05c0ac9f45
Nabeelah Yousuph <noblenab13@gmail.com>
2019-02-03 02:09:25 +0100

    Added modals for edit and delete

---
+++ script/modal.js
@@ -0,0 +1,38 @@
+// Get the modal
+var delete_modal = document.getElementById('delete-modal');
+var edit_modal = document.getElementById('edit-modal');
+// Get the button that opens the modal
+let delete_btn = document.querySelectorAll('button.delete');
+let edit_btn = document.querySelectorAll('button.edit');
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+var span1 = document.getElementsByClassName("close")[1];
+// When the user clicks the button, open the modal 
+
+[...delete_btn].map(function(el){
+	el.onclick = function() {
+      delete_modal.style.display = "block";
+  }
+}); 
+
+[...edit_btn].map(function(el){
+	el.onclick = function() {
+      edit_modal.style.display = "block";
+  }
+}); 
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  delete_modal.style.display = "none";
+}
+span1.onclick = function() {
+  edit_modal.style.display = "none";
+}
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == delete_modal || event.target == edit_modal) {
+   delete_modal.style.display = "none";
+   edit_modal.style.display = "none";
+  }
+}
--- script/vote.js
+++ script/vote.js
@@ -1,8 +1,10 @@
 document.getElementById("offices").addEventListener("change", function() {
 	if(this.value === "president"){
 		document.getElementById("president").style.display = "block";
+		document.getElementById("governor").style.display = "none";
 	}else if (this.value === "governor") {
 		document.getElementById("governor").style.display = "block";
+		document.getElementById("president").style.display = "none";
 	}else {
 		document.getElementById("president").style.display = "none";
 		document.getElementById("governor").style.display = "none";
