7fc7824272284d8c20208e2eaf207f616f316b89
Nabeelah Yousuph <noblenab13@gmail.com>
2019-02-13 18:08:58 +0100

    [Feature #163945603] Created user signup request

--- client/signup.js
+++ client/signup.js
@@ -1,1 +1,67 @@
-signup.js
+const signUp = document.querySelector('.signup-form');
+const firstName = document.getElementById('fname');
+const otherName = document.getElementById('oname');
+const email = document.getElementById('email');
+const phone = document.getElementById('phone-no');
+const profile = document.getElementById('profile-pic');
+const pswd = document.getElementById('password');
+const btn = document.getElementById('btn');
+const message = document.getElementById('info');
+const close = document.getElementsByClassName('close')[0];
+
+const displayMsg = (errors) => {
+  errors.forEach((error) => {
+    message.style.display = 'block';
+    document.querySelector('.msg').innerHTML = `${error}`;
+  });
+};
+
+close.onclick = () => {
+  message.style.display = 'none';
+};
+
+signUp.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const fName = firstName.value;
+  const oName = otherName.value;
+  const mail = email.value;
+  const phoneNo = phone.value;
+  const profilePic = profile.value;
+  const password = pswd.value;
+
+  fetch('https://politico-page.herokuapp.com/api/v1/auth/signup', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+    },
+    body: JSON.stringify({
+      'fisrtname': fName,
+      'othername': oName,
+      'email': mail,
+      'phonenumber': phoneNo,
+      'passporturl': profilePic,
+      'password': password,
+    }),
+  })
+    .then(res => res.json())
+    .then((data) => {
+      if (data.status == 201) {
+        localStorage.setItem('token', data[1]);
+        window.location.assign('home.html');
+      } else if (data.errors) {
+        const obj = data.errors;
+        const errors = Object.values(obj);
+        displayMsg(errors);
+      } else {
+        message.style.display = 'block';
+        document.querySelector('.msg').innerHTML = data.message;
+      }
+    })
+    .catch((err) => {
+      if (err) {
+        message.style.display = 'block';
+        return document.querySelector('.msg').innerHTML = 'Check your connection and try again.';
+      }
+    });
+});
--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import morgan from 'morgan';
 import dotenv from 'dotenv';
+import cors from 'cors';
 import bodyParser from 'body-parser';
 import expressValidation from 'express-validation';
 import 'babel-polyfill';
@@ -11,6 +12,15 @@ const app = express();
 app.use(morgan('dev'));
 
 const port = process.env.PORT || 3000;
+
+app.use(cors());
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, PATCH, POST, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  next();
+});
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
